4ec7b545ce0df10b339ad6462c5ebacf18811920
Ian Khrashchevskyi <39925631+iankhr@users.noreply.github.com>
2018-08-31 11:54:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -17,5 +17,16 @@ yf.pdr_override()
 data = yf.download("^GSPC", start="2010-01-01", end="2018-01-01")
 rt = np.log(data['Adj Close']).diff().dropna()
 
-gm = garch(rt*100, PQ=(1,0), poq = (2,0,1))
+gm = garch(rt*100, PQ=(1,0), poq = (1,0,1))
 gm.fit()
+
+# testing the goodness of fit
+et = gm.et
+var = np.sqrt(gm.ht)*1.96 # 5%
+prop = np.sum(np.abs(et)>var)/len(var)
+print(str(prop*100)+'%')
+var = np.sqrt(gm.ht)*2.58 # 1%
+prop = np.sum(np.abs(et)>var)/len(var)
+print(str(prop*100)+'%')
+# it means that the model underestimates the variance. if the values are bigger
+# than 5% and 1%. In particular, it manages really bad the tails
